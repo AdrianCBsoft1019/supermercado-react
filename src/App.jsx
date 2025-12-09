298d1f31ca695fe506772317897c075bcc8ee979
roderick correa <adrian.cb.soft1019@gmail.com>
2025-12-08 22:38:11 -0500

    feat: implementacion de header a la app

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductGrid from "./components/ProductGrid";
 import SobreNosotros from "./components/SobreNosotros";
 import Footer from "./components/Footer";
-import CartButton from "./components/CartButton";
 import CartPage from "./pages/CartPage";
 import { CartProvider } from "./context/CartContext";
+import Header from "./Components/Header";
 
 export default function App() {
   return (
     <CartProvider>
       <BrowserRouter>
         <div className="min-h-screen flex flex-col bg-blue-50">
-          <CartButton />
+          <Header />
 
           <main className="flex-1">
             <Routes>
